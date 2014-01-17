bda83295718148ea61d1870aa288e295b6549768
Fang-Chieh Chou <fcchou1986@gmail.com>
2014-01-17 13:26:46 -0800

    Add logo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -123,7 +123,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -203,7 +203,7 @@ latex_documents = [(
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
